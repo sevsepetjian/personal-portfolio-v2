edfdd91d7506cef770d1b052fecad0b3909b1603
Sevag Sepetjian <sevsepetjian@gmail.com>
2019-11-26 14:00:50 -0800

    Update animations

--- js/app.js
+++ js/app.js
@@ -6,14 +6,24 @@ function headerAppear() {
 
 window.addEventListener('load', headerAppear);
 
+// Mobile
+if(window.screen.width <= 768) {
+    function projectsAppearMobile() {
+        const projects = document.querySelector('.projects');
+    
+        projects.classList.add('projects__appear');
+    }
+}
+window.addEventListener('load', projectsAppearMobile);
+
+// Desktop
 function projectsAppear() {
     const projects = document.querySelector('.projects');
     let projectsPosition = projects.getBoundingClientRect().top;
     let screenPosition = window.innerHeight / 1.3;
-    
+
     if(projectsPosition < screenPosition) {
         projects.classList.add('projects__appear');
     }
 }
-
 window.addEventListener('scroll', projectsAppear);
